de3a9619c326ae172dd6a9b48efff5982b7deada
tornikegomareli <gomarelidevelopment@gmail.com>
2024-06-17 17:41:26 +0400

    removed libraries from package

--- Package.swift
+++ Package.swift
@@ -5,10 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "TokyoNight-XcodeTheme",
-    dependencies: [
-        .package(url: "https://github.com/JohnSundell/Files.git", from: "4.0.0"),
-        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0")
-    ],
     targets: [
         .target(name: "TokyoNight-XcodeTheme"),
     ]
